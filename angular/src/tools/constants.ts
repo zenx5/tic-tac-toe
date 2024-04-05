0277f24451e801857ab0ff29075ab7cfa215f088
Octavio Martinez <omartinez1618@gmail.com>
2024-04-05 15:53:54 -0400

    fix in angular

---
+++ angular/src/tools/constants.ts
@@ -0,0 +1,18 @@
+export const winsArray = [
+    [0,1,2],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [3,4,5],
+    [6,4,2],
+    [6,7,8]
+]
+export const USER_X = 1
+export const USER_O = 2
+export const USER_NONE = 0
+export const DRAW = 3
+export const TAG_X = "X"
+export const TAG_O = "O"
+export const TAG_NONE = ""
